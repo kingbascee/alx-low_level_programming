5fee754502be861cc5d39e55575b6e2e912eda56
kingbascee <kingbascee@gmail.com>
2022-12-09 11:29:07 -0800

    The success combination in business is: Do what you do better... and: do more of what you do

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,40 +1,33 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- * main -  prints all possible different combinations of three digits.
- * Return: Always 0 (Success)
- */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int ones = '0';
-	int tens = '0';
-	int hundreds = '0';
-
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
-	{
-		for (tens = '0'; tens <= '9'; tens++)
-		{
-			for (ones = '0'; ones <= '9'; ones++)
-			{
-	if
-	(!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens))
-	 )
-			/*eliminates repitition*/
-	{	putchar(hundreds);
-		putchar(tens);
-		putchar(ones);
-	if
-	(!(ones == '9' && hundreds == '7' && tens == '8'))
-				/*adds commas and spaces*/
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int d, p, q;
+for (d = '0'; d < '9'; d++)
+{
+for (p = d + 1; p <= '9'; p++)
+{
+for (q = p + 1; q <= '9'; q++)
+{
+if ((p != d) != q)
+{
+putchar(d);
+putchar(p);
+putchar(q);
+if (d == '7' && p == '8')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
