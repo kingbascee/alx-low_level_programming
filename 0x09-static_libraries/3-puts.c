18d2f727e26131c9a203473a0e908c52dda24fb6
kingbascee <kingbascee@gmail.com>
2023-01-06 14:38:14 -0800

    task 1 fixed

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * _islower - checks if a character is lowercase if YES it returns 1
- * otherwise returns 0 just like islower() function in ctype.h
- * @c: Is the int value to be compared with the ASCII value
- * Return: 0 if successful.
+ * _puts - print a string to stdout
+ * @str: pointer parameter
  */
-int _islower(int c)
+
+void _puts(char *str)
 {
-	if (c >= 97 && c <= 122)
-		return (1);
-	else
-		return (0);
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
 }
